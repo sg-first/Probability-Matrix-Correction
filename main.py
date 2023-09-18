095d1d92efd179172a5c514df3d451b1413045fc
3A <524019351@qq.com>
2023-09-18 10:56:27 +0800

    遍历所有组合取最大值

--- main.py
+++ main.py
@@ -8,38 +8,10 @@ col2 = {'A': 0.8, 'B': 0.11, 'C': 0.09, 'ID': 1}
 col3 = {'A': 0.7, 'B': 0.15, 'C': 0.15, 'ID': 2}
 probMat = [col1, col2, col3]
 
-
-def getChooseMaxNum(probMat: list, choose, choosedList):
-    num = 0
-    for col in probMat:
-        maxValue = None
-        maxChoose = None
-        for _choose in allChoose:
-            if _choose not in choosedList:
-                if maxValue is None or col[_choose] > maxValue:
-                    maxValue = col[_choose]
-                    maxChoose = _choose
-        if maxChoose == choose:
-            num += 1
-    return num
-
-
-def getMaxNumChoose(probMat: list, choosedList):
-    maxValue = None
-    maxChoose = None
-    for choose in allChoose:
-        if choose not in choosedList:
-            value = getChooseMaxNum(probMat, choose, choosedList)
-            print(choose, value)
-            if maxValue is None or value > maxValue:
-                maxValue = value
-                maxChoose = choose
-    return maxChoose
-
 def calcExp(answer):
     def calcExpForChoose(choose):  # 计算选某个选项的题正确的期望
         answerA = []
-        for i in answer.values():
+        for i in answer:
             if i['choose'] == choose:
                 answerA.append(i['prob'])
         answerAExp = 0
@@ -60,51 +32,30 @@ def calcExp(answer):
         exp += calcExpForChoose(choose)
     return exp
 
+def getChooseAndProbFromProbMat(sub, choose):
+    col = probMat[sub]
+    return {'choose':choose, 'prob':col[choose]}
 
-# 按选项顺序选题
-def addAnswerByChoose(answer: dict, choose: str, chooseTimes: int):
-    probMatSortByA = copy.copy(probMat)
-    probMatSortByA.sort(key=lambda i: i[choose], reverse=True)
-    print('sort by', choose, probMatSortByA)
-    # 在probMatSortByA中选择未回答的前chooseTimes个问题，选择choose选项
-    allAnsweredID = list(answer.keys())
-    i = 0  # 当前已选该选项数量
-    for col in probMatSortByA:
-        if col['ID'] not in allAnsweredID and i < chooseTimes:
-            print('do choose', col)
-            answer[col['ID']] = {'choose': choose, 'prob': col[choose]}
-            i += 1
-    return answer
-
-
-def getMaxExpAnswer():
-    global maxExp, maxExpAnswer
-    maxExp = None
-    maxExpAnswer = None
-    def recu(answer, chooseSub, leftTimes, choosedList):
-        print(len(answer))
-        if chooseSub == len(allChoose) or len(answer) == len(probMat):
-            exp = calcExp(answer)
-            global maxExp, maxExpAnswer
-            if maxExp is None or exp > maxExp:
-                maxExp = exp
-                maxExpAnswer = answer
+def calcAllAnswer():
+    allAnswer = []
+    def recu(answer: list):
+        if len(answer) == len(probMat):
+            print(answer)
+            allAnswer.append(answer)
         else:
-            maxChoose = getMaxNumChoose(probMat, choosedList)
-            print(maxChoose)
-            nextChoosedList = copy.copy(choosedList)
-            nextChoosedList.append(maxChoose)
-            if chooseSub == len(allChoose) - 1:  # 最后一次，要让所有题都选上
+            for choose in allChoose:
                 nextAnswer = copy.copy(answer)
-                nextAnswer = addAnswerByChoose(nextAnswer, maxChoose, leftTimes)
-                recu(nextAnswer, chooseSub + 1, len(probMat) - len(nextAnswer), nextChoosedList)
-            else:
-                for i in range(leftTimes + 1):
-                    nextAnswer = copy.copy(answer)
-                    nextAnswer = addAnswerByChoose(nextAnswer, maxChoose, i)
-                    recu(nextAnswer, chooseSub + 1, len(probMat) - len(nextAnswer), nextChoosedList)
+                nextAnswer.append(getChooseAndProbFromProbMat(len(answer), choose))
+                recu(nextAnswer)
+    recu([])
+    return allAnswer
 
-    recu({}, 0, len(probMat), [])
-    return maxExp, maxExpAnswer
-
-print(getMaxExpAnswer())
+allAnswer = calcAllAnswer()
+maxExp = None
+maxExpAnswer = None
+for answer in allAnswer:
+    exp = calcExp(answer)
+    if maxExp is None or exp > maxExp:
+        maxExp = exp
+        maxExpAnswer = answer
+print(maxExp, maxExpAnswer)
