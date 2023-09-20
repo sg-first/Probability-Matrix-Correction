bedd945a565f8ff02750c0acc86cf3ccf13a2db4
3A <524019351@qq.com>
2023-09-20 22:53:51 +0800

    原始解法改进版

--- main.py
+++ main.py
@@ -1,17 +1,40 @@
 import permutation
 
+def calcAnswerAExp(answerA, maxTimes):
+    # n次试验，事件发生的概率各为answerA中的元素，求成功次数期望
+    # 注：最大成功maxTimes次，比这个大的不算
+    global probSum, dist
+    probSum = 0
+    dist = {}
+    for i in range(maxTimes + 1):
+        dist[i] = 0
+
+    def recu(sub, thisProb, successTimes):
+        global probSum, dist
+        if sub == len(answerA):
+            if successTimes <= maxTimes:
+                probSum += thisProb
+                dist[successTimes] += thisProb
+        else:
+            recu(sub + 1, thisProb * (1 - answerA[sub]), successTimes)
+            recu(sub + 1, thisProb * answerA[sub], successTimes + 1)
+    recu(0, 1, 0)
+    # 计算期望
+    exp = 0
+    for num, probNumerator in dist.items():
+        prob = probNumerator / probSum
+        exp += prob * num
+    return exp
+
 def calcExp(answer):
     def calcExpForChoose(choose):  # 计算选某个选项的题正确的期望
         answerA = []
         for i in answer:
             if i['choose'] == choose:
                 answerA.append(i['prob'])
-        answerAExp = 0
-        answerA.sort(reverse=True)
-        i = 0
-        while i < permutation.prior[choose] and i < len(answerA):
-            answerAExp += answerA[i]
-            i += 1
+        # answerA.sort(reverse=True)
+        answerAExp = calcAnswerAExp(answerA, permutation.prior[choose])
+        print(choose, answerAExp)
         # 根据先验确定期望最小值
         expMin = len(answerA) - (len(permutation.probMat) - permutation.prior[choose])
         if answerAExp < expMin:
